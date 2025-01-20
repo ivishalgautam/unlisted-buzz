1ccf86cc4ee01e27d51e3dbd23dcdce5d6c291f8
Vishal Gautam <ivishalgautamm@gmail.com>
2025-01-20 10:45:03 +0530

    fixed not found modules

--- dashboard/src/app/users/edit/[id]/page.jsx
+++ dashboard/src/app/users/edit/[id]/page.jsx
@@ -2,14 +2,14 @@
 import UserForm from "@/components/forms/user";
 import PageContainer from "@/components/layout/page-container";
 import { Heading } from "@/components/ui/heading";
-import { updateUser } from "@/server/users";
+import { updateSector } from "@/server/sector";
 import { useMutation } from "@tanstack/react-query";
 import React from "react";
 import { toast } from "sonner";
 
 export default function Page({ params: { id } }) {
   const updateMutation = useMutation({
-    mutationFn: (data) => updateUser(data, id),
+    mutationFn: (data) => updateSector(data, id),
     onSuccess: () => {},
     onSettled: () => toast.success("Updated"),
   });
