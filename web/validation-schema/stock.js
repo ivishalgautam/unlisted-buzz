cacfd28739022b126e4c9f430ed7c8db0d0946f8
Vishal Gautam <ivishalgautamm@gmail.com>
2025-02-11 15:31:32 +0530

    fixed validations

--- web/validation-schema/stock.js
+++ web/validation-schema/stock.js
@@ -4,11 +4,12 @@ export const newStockSchema = z.object({
   share_id: z.string({ required_error: "Share is required*" }).uuid(),
   quantity: z
     .number({ required_error: "Please enter quantity*" })
-    .int("Quantity must be an integer")
-    .positive("Quantity must be greater than 0"),
+    .positive("Quantity must be greater than 0")
+    .max(2147483647, { message: "Quantity must be less than 2147483647" }),
   purchase_price: z
     .number({ required_error: "Please enter purchase price*" })
-    .positive("Purchase price must be greater than 0"),
+    .positive("Purchase price must be greater than 0")
+    .max(2147483647, { message: "Quantity must be less than 2147483647" }),
   date_of_purchase: z
     .string({ required_error: "Date of purchase is required*" })
     .refine((date) => !isNaN(Date.parse(date)), "Invalid date format"),
