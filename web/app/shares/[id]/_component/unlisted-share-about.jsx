dc4e72925c1ea9a096afeff1ec35ebecc62e3f58
Vishal Gautam <ivishalgautamm@gmail.com>
2025-04-07 13:58:36 +0530

    fixed about section show more button bug

--- web/app/shares/[id]/_component/unlisted-share-about.jsx
+++ web/app/shares/[id]/_component/unlisted-share-about.jsx
@@ -13,6 +13,8 @@ import { useState } from "react";
 
 export function ShareAbout({ companyName, about }) {
   const [isShowMore, setIsShowMore] = useState(false);
+  const aboutLength = about.split(/\s+/).length;
+  console.log(about.split(/\s+/));
 
   return (
     <Card>
@@ -24,22 +26,26 @@ export function ShareAbout({ companyName, about }) {
         <div
           dangerouslySetInnerHTML={{ __html: about }}
           className={cn(
-            "prose-sm h-44 !min-w-full overflow-hidden relative before:absolute before:w-full before:bg-gradient-to-t before:from-white before:to-transparent before:h-1/2 before:bottom-0 before:left-0",
+            "prose-sm !min-w-full overflow-hidden relative h-auto",
             {
               "h-auto before:hidden": isShowMore,
+              "h-44 before:absolute before:w-full before:bg-gradient-to-t before:from-white before:to-transparent before:h-1/2 before:bottom-0 before:left-0":
+                aboutLength > 150 && !isShowMore,
             }
           )}
         ></div>
-        <div className="text-center">
-          <Button
-            type="button"
-            // variant="ghost"
-            className=""
-            onClick={() => setIsShowMore(!isShowMore)}
-          >
-            {isShowMore ? "Show less" : "Show more"}
-          </Button>
-        </div>
+        {aboutLength > 150 && (
+          <div className="text-center">
+            <Button
+              type="button"
+              // variant="ghost"
+              className=""
+              onClick={() => setIsShowMore(!isShowMore)}
+            >
+              {isShowMore ? "Show less" : "Show more"}
+            </Button>
+          </div>
+        )}
       </CardContent>
     </Card>
   );
